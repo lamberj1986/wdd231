192213c1721abafcccbe5d692cc6e3f55c007363
lamberj1986 <lamberj1986@gmail.com>
2024-11-12 22:02:04 -0500

    New files for week 2, work is in progress.

---
+++ scripts/prophets.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+    const cards = document.querySelector('#cards');
+
+    async function getProphetData() {
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.table(data.prophets);
+        displayProphets(data.prophets);
+      }
+      
+      getProphetData();
+});
